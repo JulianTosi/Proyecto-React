36fe9733174b03f6f9a57a2b61a211ab939d4767
Julian Tosi <julian-tosi@hotmail.com>
2022-08-30 20:00:27 -0300

    primero cambios

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+const NavBar = () => {
+    return (
+        <div>
+            <div className="propNav">
+                <img className="pruebaImg" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f8903b05-cb23-4f4e-bf68-9546c4d368e7/d5difqb-f7c02d4a-1493-44a9-a4ce-502a7e7e928f.png/v1/fill/w_603,h_601,strp/random_logo_i_was_bored_designing_by_shadow3vi1_d5difqb-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjAxIiwicGF0aCI6IlwvZlwvZjg5MDNiMDUtY2IyMy00ZjRlLWJmNjgtOTU0NmM0ZDM2OGU3XC9kNWRpZnFiLWY3YzAyZDRhLTE0OTMtNDRhOS1hNGNlLTUwMmE3ZTdlOTI4Zi5wbmciLCJ3aWR0aCI6Ijw9NjAzIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.tiXjlMj4qj8txjwv4x9D2CiqwjcXAL2dLlbihSTjiY4" />
+
+                
+                <ul className="listaNav">
+                    <li>
+                        <a href="#">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="#">Productos</a>
+                    </li>
+                    <li>
+                        <a href="#">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
